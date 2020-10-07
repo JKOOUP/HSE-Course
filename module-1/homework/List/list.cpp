0c749ad5f096ed66fddbd007238bd2d95d9d5d50
JKOOUP <evdgenij@mail.ru>
2020-10-08 01:32:17 +0300

    first realization, but in one file

--- module-1/homework/List/list.cpp
+++ module-1/homework/List/list.cpp
@@ -1,3 +1,21 @@
 #include "list.h"
 
-// Your code goes here...
+// using namespace task;
+
+// int main() {
+// 	list a, b;
+
+// 	for (int i = 0; i < 5; i++) {
+// 		a.push_back(i);
+// 		b.push_front(i);
+// 	}
+
+// 	a.print();
+// 	b.print();
+
+// 	a.swap(b);
+
+// 	a.print();
+// 	b.print();
+// 	return 0;
+// }
