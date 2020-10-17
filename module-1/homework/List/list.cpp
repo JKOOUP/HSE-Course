ff22a1fd278df754aed6f01be0ebde4f43537297
JKOOUP <evdgenij@mail.ru>
2020-10-17 17:31:48 +0300

    fix some warnings

--- module-1/homework/List/list.cpp
+++ module-1/homework/List/list.cpp
@@ -1,72 +1,70 @@
 #include "list.h"
 
-using namespace task;
+task::Node::Node(): prev_element(nullptr), next_element(nullptr), value(0) {};
 
-Node::Node(): prev_element(nullptr), next_element(nullptr), value(0) {};
-
-Node::Node(Node* const left, Node* const right, int elem) {
+task::Node::Node(Node* const left, Node* const right, int elem) {
     prev_element = left;
     next_element = right;
     value = elem;
 };
 
-Node::Node(const Node& node) {
+task::Node::Node(const Node& node) {
     prev_element = node.prev_element;
     next_element = node.next_element;
     value = node.value;
 };
 
-Node::~Node() {
+task::Node::~Node() {
     prev_element = next_element = nullptr;
     delete prev_element;
     delete next_element;
 };
 
-Node& Node::operator=(const Node& node) {
+task::Node& task::Node::operator=(const Node& node) {
     next_element = node.next_element;
     prev_element = node.prev_element; 
     value = node.value;
     return *this;
 }
 
-void Node::set_prev_element(Node* const prev) {
+void task::Node::set_prev_element(Node* const prev) {
     prev_element = prev;
 }
 
-Node* Node::get_ptr_to_prev_element() const {
+task::Node* task::Node::get_ptr_to_prev_element() const {
     return prev_element;
 }
 
-void Node::set_next_element(Node* const next) {
+void task::Node::set_next_element(Node* const next) {
     next_element = next;
 }
 
-Node* Node::get_ptr_to_next_element() const {
+task::Node* task::Node::get_ptr_to_next_element() const {
     return next_element;
 }
 
-const int& Node::get_value() const {
+const int& task::Node::get_value() const {
     return value;
 }
 
-int& Node::get_value() {
+int& task::Node::get_value() {
     return value;
 }
 
-void Node::swap(Node* &a, Node* &b) {
+void task::Node::swap(Node* &a, Node* &b) {
     Node* c = a;
     a = b;
     b = c;
 }
 
-list::list(): head(nullptr), tail(nullptr), list_size(0) {};
+task::list::list(): head(nullptr), tail(nullptr), list_size(0) {};
 
-list::list(std::size_t count, const int& value): list_size(count) {
-    head = new Node(nullptr, nullptr, value);
-    Node* curr_node = head;
+task::list::list(std::size_t count, const int& value): list_size(count) {
+    head = new task::Node(nullptr, nullptr, value);
+    task::Node* curr_node = head;
 
     for (std::size_t i = 0; i < count - 1; i++) {
-        Node* new_node = new Node(nullptr, nullptr, value);
+        task::Node* new_node = new task::Node(nullptr, nullptr, value);
         new_node->set_prev_element(curr_node);
         curr_node->set_next_element(new_node);
         curr_node = new_node;
@@ -74,16 +72,16 @@ list::list(std::size_t count, const int& value): list_size(count) {
     tail = curr_node;
 }   
 
-list::list(const list& other) {
+task::list::list(const task::list& other) {
     this->list_size = other.size();
 
-    Node* curr_node = other.head;
-    Node* prev_node = new Node(nullptr, nullptr, other.head->get_value());
+    task::Node* curr_node = other.head;
+    task::Node* prev_node = new task::Node(nullptr, nullptr, other.head->get_value());
     head = prev_node;
 
     for (std::size_t i = 0; i < other.size() - 1; i++) {
         curr_node = curr_node->get_ptr_to_next_element();
-        Node* new_node = new Node(nullptr, nullptr, curr_node->get_value());
+        task::Node* new_node = new task::Node(nullptr, nullptr, curr_node->get_value());
         
         prev_node->set_next_element(new_node);
         new_node->set_prev_element(prev_node);
@@ -94,10 +92,10 @@ list::list(const list& other) {
     this->tail = prev_node;
 }
 
-list::~list() {
-    Node* curr_node = tail;
+task::list::~list() {
+    task::Node* curr_node = tail;
     while (curr_node != head) {
-        Node* node_to_delete = curr_node;
+        task::Node* node_to_delete = curr_node;
         curr_node = curr_node->get_ptr_to_prev_element();
         delete node_to_delete;
     }
@@ -106,17 +104,17 @@ list::~list() {
     delete curr_node;
 }
 
-list& list::operator=(const list& other) {
+task::list& task::list::operator=(const list& other) {
     this->clear();
 
     this->list_size = other.size();
-    Node* curr_node = other.head;
-    Node* prev_node = new Node(nullptr, nullptr, other.head->get_value());
+    task::Node* curr_node = other.head;
+    task::Node* prev_node = new task::Node(nullptr, nullptr, other.head->get_value());
     head = prev_node;
 
     for (std::size_t i = 0; i < other.size() - 1; i++) {
         curr_node = curr_node->get_ptr_to_next_element();
-        Node* new_node = new Node(nullptr, nullptr, curr_node->get_value());
+        task::Node* new_node = new task::Node(nullptr, nullptr, curr_node->get_value());
         
         prev_node->set_next_element(new_node);
         new_node->set_prev_element(prev_node);
@@ -128,34 +126,34 @@ list& list::operator=(const list& other) {
     return *this;
 }
 
-int& list::front() {
+int& task::list::front() {
     return head->get_value();
 }
 
-const int& list::front() const {
+const int& task::list::front() const {
     return head->get_value();
 }
 
-int& list::back() {
+int& task::list::back() {
     return tail->get_value();
 }
 
-const int& list::back() const {
+const int& task::list::back() const {
     return tail->get_value();
 }
 
-bool list::empty() const {
+bool task::list::empty() const {
     return (list_size == 0);
 }
 
-std::size_t list::size() const {
+std::size_t task::list::size() const {
     return this->list_size;
 }
 
-void list::clear() {
-    Node* curr_node = tail;
+void task::list::clear() {
+    task::Node* curr_node = tail;
     while (curr_node != head) {
-        Node* node_to_delete = curr_node;
+        task::Node* node_to_delete = curr_node;
         curr_node = curr_node->get_ptr_to_prev_element();
         delete node_to_delete;
     }
@@ -164,8 +162,8 @@ void list::clear() {
     list_size = 0;
 }
 
-void list::push_back(const int& value) {
-    Node* new_node = new Node(nullptr, nullptr, value);
+void task::list::push_back(const int& value) {
+    task::Node* new_node = new task::Node(nullptr, nullptr, value);
 
     if (list_size != 0) {
         new_node->set_prev_element(tail);
@@ -179,11 +177,11 @@ void list::push_back(const int& value) {
     list_size++;
 }
 
-void list::pop_back() {
+void task::list::pop_back() {
     if (list_size == 0)
         return;
 
-    Node* node_to_delete = tail;
+    task::Node* node_to_delete = tail;
     if (list_size == 1)
         head = tail = nullptr;
     else {
@@ -195,8 +193,8 @@ void list::pop_back() {
     list_size--;
 }
 
-void list::push_front(const int& value) {
-    Node* new_node = new Node(nullptr, nullptr, value);
+void task::list::push_front(const int& value) {
+    task::Node* new_node = new task::Node(nullptr, nullptr, value);
     
     if (list_size != 0) {
         new_node->set_next_element(head);
@@ -210,11 +208,11 @@ void list::push_front(const int& value) {
     list_size++;
 }
 
-void list::pop_front() {
+void task::list::pop_front() {
     if (list_size == 0)
         return;
 
-    Node* node_to_delete = head;
+    task::Node* node_to_delete = head;
     if (list_size == 1)
         head = tail = nullptr;
     else {
@@ -226,7 +224,7 @@ void list::pop_front() {
     list_size--;
 }
 
-void list::resize(std::size_t count) {
+void task::list::resize(std::size_t count) {
     while (list_size > count)
         this->pop_back();
 
@@ -234,23 +232,23 @@ void list::resize(std::size_t count) {
         this->push_back(0);
 }
 
-void list::swap(list& other) {
-    Node::swap(this->head, other.head);
-    Node::swap(this->tail, other.tail);
+void task::list::swap(task::list& other) {
+    task::Node::swap(this->head, other.head);
+    task::Node::swap(this->tail, other.tail);
 
-    int tmp_size = this->list_size;
+    std::size_t tmp_size = this->list_size;
     this->list_size = other.list_size;
     other.list_size = tmp_size;
 }
 
-void list::remove(const int& value) {
+void task::list::remove(const int& value) {
     while (list_size > 0 && head->get_value() == value)
         this->pop_front();
 
     while (list_size > 0 && tail->get_value() == value)
         this->pop_back();
     
-    for (Node* curr_node = head; curr_node != tail; curr_node = curr_node->get_ptr_to_next_element()) {
+    for (task::Node* curr_node = head; curr_node != tail; curr_node = curr_node->get_ptr_to_next_element()) {
         if (curr_node->get_value() != value)
             continue;
 
@@ -258,8 +256,8 @@ void list::remove(const int& value) {
     }
 }
 
-void list::unique() {
-    for (Node* curr_node = head; curr_node != nullptr; curr_node = curr_node->get_ptr_to_next_element()) {
+void task::list::unique() {
+    for (task::Node* curr_node = head; curr_node != nullptr; curr_node = curr_node->get_ptr_to_next_element()) {
         if (list_size <= 1)
             break; 
 
@@ -275,12 +273,12 @@ void list::unique() {
     }
 }
 
-void list::sort() {
+void task::list::sort() {
     *this = merge_sort(*this);
 }
 
-void list::erase_element_prev(Node* &elem) {
-    Node* node_to_delete = elem;
+void task::list::erase_element_prev(task::Node* &elem) {
+    task::Node* node_to_delete = elem;
     elem->prev_element->set_next_element(elem->next_element);
     elem->next_element->set_prev_element(elem->prev_element);
 
@@ -289,8 +287,8 @@ void list::erase_element_prev(Node* &elem) {
     list_size--;
 }
 
-void list::erase_element_next(Node* &elem) {
-    Node* node_to_delete = elem;
+void task::list::erase_element_next(task::Node* &elem) {
+    task::Node* node_to_delete = elem;
     elem->prev_element->set_next_element(elem->next_element);
     elem->next_element->set_prev_element(elem->prev_element);
 
@@ -299,12 +297,12 @@ void list::erase_element_next(Node* &elem) {
     list_size--;
 }
 
-list list::merge_sort(const list l) {
+task::list task::list::merge_sort(const task::list l) {
     if (l.size() <= 1)
         return l;
     
-    list left, right;
-    Node* curr_element = l.head;
+    task::list left, right;
+    task::Node* curr_element = l.head;
 
 
     for (std::size_t i = 0; i < (l.size() + 1) / 2; i++, curr_element = curr_element->get_ptr_to_next_element())
@@ -319,11 +317,11 @@ list list::merge_sort(const list l) {
     return merge(left, right);
 }
 
-list list::merge(const list& a, const list& b) {
-    list result;
+task::list task::list::merge(const task::list& a, const task::list& b) {
+    task::list result;
 
-    Node* first_curr_element = a.head;
-    Node* second_curr_element = b.head;
+    task::Node* first_curr_element = a.head;
+    task::Node* second_curr_element = b.head;
 
     while (first_curr_element != nullptr && second_curr_element != nullptr) {
         if (first_curr_element->get_value() <= second_curr_element->get_value()) {
