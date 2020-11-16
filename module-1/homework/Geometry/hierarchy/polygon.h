35a5a8604d4ca6822d7fd1b601be7847f8e66fe8
JKOOUP <evdgenij@mail.ru>
2020-11-16 12:35:12 +0300

    fix tabs

--- module-1/homework/Geometry/hierarchy/polygon.h
+++ module-1/homework/Geometry/hierarchy/polygon.h
@@ -5,53 +5,53 @@ class Polygon : public Shape {
 
 public:
 
-	Polygon() = default;
+    Polygon() = default;
 
-	Polygon(const std::vector<Point>& v);
+    Polygon(const std::vector<Point>& v);
 
-	Polygon(const Polygon& other);
+    Polygon(const Polygon& other);
 
-	Polygon& operator=(const Polygon& other);
+    Polygon& operator=(const Polygon& other);
 
-	std::size_t verticesCount() const;
+    std::size_t verticesCount() const;
 
-	std::vector<Point> getVertices() const;
+    std::vector<Point> getVertices() const;
 
-	std::vector<double> getAngles() const;
+    std::vector<double> getAngles() const;
 
-	bool isConvex() const;
+    bool isConvex() const;
 
-	virtual bool operator==(const Shape& other) override;
+    virtual bool operator==(const Shape& other) override;
 
-	virtual bool isCongruentTo(const Shape& other) override;
+    virtual bool isCongruentTo(const Shape& other) override;
 
-	virtual bool isSimilarTo(const Shape& other) override;	
+    virtual bool isSimilarTo(const Shape& other) override;  
 
-	virtual double perimeter() override;
+    virtual double perimeter() override;
 
-	virtual double area() override;
+    virtual double area() override;
 
-	virtual bool containsPoint(const Point& p) override;
+    virtual bool containsPoint(const Point& p) override;
 
-	virtual void rotate(const Point& center, double ang) override;
+    virtual void rotate(const Point& center, double ang) override;
 
-	virtual void reflex(const Point& center) override;
+    virtual void reflex(const Point& center) override;
 
-	virtual void reflex(const Line& axis) override;
+    virtual void reflex(const Line& axis) override;
 
-	virtual void scale(const Point& center, double coef) override;
+    virtual void scale(const Point& center, double coef) override;
 
-	friend std::ostream& operator<<(std::ostream& out, const Polygon& p);
+    friend std::ostream& operator<<(std::ostream& out, const Polygon& p);
 
 protected:
 
-	std::vector<Point> vert; 
+    std::vector<Point> vert; 
 
-	static std::size_t get_corresponding_vertice(const Polygon& a, 
+    static std::size_t get_corresponding_vertice(const Polygon& a, 
                                                  const Polygon& b);
 
-	static double get_similarity_coefficient(const Polygon& a, 
+    static double get_similarity_coefficient(const Polygon& a, 
                                              const Polygon& b, 
-											 std::size_t pos1, 
-											 std::size_t pos2);
+                                             std::size_t pos1, 
+                                             std::size_t pos2);
 };
